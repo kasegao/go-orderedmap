10b800d51cfa222b68cbf210c229aaac5ad62c42
kasegao <shimomura@hapis.k.u-tokyo.ac.jp>
2022-10-26 23:50:02 +0900

    refac: remove 'values' field

--- omap.go
+++ omap.go
@@ -2,16 +2,16 @@ package omap
 
 import "fmt"
 
-type node[K comparable] struct {
-	key  K
-	prev *node[K]
-	next *node[K]
+type node[K comparable, V any] struct {
+	key   K
+	value V
+	prev  *node[K, V]
+	next  *node[K, V]
 }
 
 type OrderedMap[K comparable, V any] struct {
-	root   *node[K]
-	nodes  map[K]*node[K]
-	values map[K]V
+	root  *node[K, V]
+	nodes map[K]*node[K, V]
 }
 
 type Entry[K comparable, V any] struct {
@@ -19,45 +19,53 @@ type Entry[K comparable, V any] struct {
 	Value V
 }
 
+func newNode[K comparable, V any](key K, value V, prev *node[K, V], next *node[K, V]) *node[K, V] {
+	return &node[K, V]{key: key, value: value, prev: prev, next: next}
+}
+
+func (n *node[K, V]) entry() *Entry[K, V] {
+	return &Entry[K, V]{Key: n.key, Value: n.value}
+}
+
 // New constructs an OrderedMap.
 func New[K comparable, V any]() *OrderedMap[K, V] {
-	root := &node[K]{}
+	root := &node[K, V]{}
 	root.prev = root
 	root.next = root
 	om := &OrderedMap[K, V]{
-		root:   root,
-		nodes:  make(map[K]*node[K]),
-		values: make(map[K]V),
+		root:  root,
+		nodes: make(map[K]*node[K, V]),
 	}
 	return om
 }
 
 // Contains checks if a key is in the map.
-func (om *OrderedMap[K, V]) Contains(key K) bool {
-	_, ok := om.values[key]
+func (om *OrderedMap[K, V]) Contains(key K) (ok bool) {
+	_, ok = om.nodes[key]
 	return ok
 }
 
 // Get returns the value for a key.
-func (om *OrderedMap[K, V]) Get(key K) (V, bool) {
-	v, ok := om.values[key]
-	return v, ok
+func (om *OrderedMap[K, V]) Get(key K) (value V, ok bool) {
+	if n, ok := om.nodes[key]; ok {
+		return n.value, true
+	}
+	return
 }
 
 // GetAt returns the key and value at a given index.
-func (om *OrderedMap[K, V]) GetAt(index int) (*Entry[K, V], bool) {
+func (om *OrderedMap[K, V]) GetAt(index int) (e *Entry[K, V], ok bool) {
 	length := om.Len()
 	if length == 0 {
 		return nil, false
 	}
 
-	var curr *node[K]
 	root := om.root
 	idx := index % length
 	if idx < 0 {
 		idx += length
 	}
-	curr = root.next
+	curr := root.next
 	for i := 0; i < idx; i++ {
 		curr = curr.next
 		if curr == root {
@@ -65,27 +73,22 @@ func (om *OrderedMap[K, V]) GetAt(index int) (*Entry[K, V], bool) {
 		}
 	}
 
-	return newEntry(curr.key, om.values[curr.key]), true
+	return curr.entry(), true
 }
 
 // Set sets the value for a key.
 func (om *OrderedMap[K, V]) Set(key K, value V) {
-	if om.Contains(key) {
-		om.values[key] = value
+	if n, ok := om.nodes[key]; ok {
+		n.value = value
 		return
 	}
 
 	root := om.root
 	tail := root.prev
-	n := &node[K]{
-		key:  key,
-		prev: tail,
-		next: root,
-	}
+	n := newNode(key, value, tail, root)
 	tail.next = n
 	root.prev = n
 	om.nodes[key] = n
-	om.values[key] = value
 }
 
 // Insert inserts a key and value at a given index.
@@ -98,7 +101,7 @@ func (om *OrderedMap[K, V]) Insert(index int, key K, value V) {
 	om.Delete(key)
 
 	neibor := om.nodes[e.Key]
-	n := &node[K]{key: key}
+	n := newNode(key, value, nil, nil)
 	if index >= 0 {
 		n.prev = neibor.prev
 		n.next = neibor
@@ -111,57 +114,49 @@ func (om *OrderedMap[K, V]) Insert(index int, key K, value V) {
 		neibor.next = n
 	}
 	om.nodes[key] = n
-	om.values[key] = value
 }
 
 // Clear clears the map.
 func (om *OrderedMap[K, V]) Clear() {
 	for key := range om.nodes {
 		delete(om.nodes, key)
-		delete(om.values, key)
 	}
 }
 
 // Delete deletes the value for a key.
-func (om *OrderedMap[K, V]) Delete(key K) bool {
+func (om *OrderedMap[K, V]) Delete(key K) (ok bool) {
 	n, ok := om.nodes[key]
 	if ok {
 		n.prev.next = n.next
 		n.next.prev = n.prev
 		delete(om.nodes, key)
-		delete(om.values, key)
 	}
 	return ok
 }
 
 // DeleteAt deletes the key and value at a given index.
-func (om *OrderedMap[K, V]) DeleteAt(index int) bool {
+func (om *OrderedMap[K, V]) DeleteAt(index int) (ok bool) {
 	e, ok := om.GetAt(index)
 	if !ok {
 		return false
 	}
-
 	return om.Delete(e.Key)
 }
 
 // Pop deletes and returns the key and value for a key.
-func (om *OrderedMap[K, V]) Pop(key K) (*Entry[K, V], bool) {
-	v, ok := om.Get(key)
-	if !ok {
-		return nil, false
+func (om *OrderedMap[K, V]) Pop(key K) (e *Entry[K, V], ok bool) {
+	if n, ok := om.nodes[key]; ok {
+		return n.entry(), om.Delete(key)
 	}
-
-	return newEntry(key, v), om.Delete(key)
+	return
 }
 
 // PopAt deletes and returns the key and value at a given index.
-func (om *OrderedMap[K, V]) PopAt(index int) (*Entry[K, V], bool) {
-	e, ok := om.GetAt(index)
-	if !ok {
-		return nil, false
+func (om *OrderedMap[K, V]) PopAt(index int) (e *Entry[K, V], ok bool) {
+	if e, ok := om.GetAt(index); ok {
+		return e, om.Delete(e.Key)
 	}
-
-	return e, om.Delete(e.Key)
+	return
 }
 
 // Len returns the number of items in the map.
@@ -177,13 +172,6 @@ func (om *OrderedMap[K, V]) String() string {
 	return fmt.Sprintf("OrderedMap%v", builder)
 }
 
-func newEntry[K comparable, V any](key K, value V) *Entry[K, V] {
-	return &Entry[K, V]{
-		Key:   key,
-		Value: value,
-	}
-}
-
 // Keys returns the keys in the map.
 func (om *OrderedMap[K, V]) Keys() []K {
 	keys := make([]K, 0, om.Len())
@@ -197,7 +185,7 @@ func (om *OrderedMap[K, V]) Keys() []K {
 func (om *OrderedMap[K, V]) Values() []V {
 	values := make([]V, 0, om.Len())
 	for n := om.root.next; n != om.root; n = n.next {
-		values = append(values, om.values[n.key])
+		values = append(values, n.value)
 	}
 	return values
 }
@@ -206,23 +194,22 @@ func (om *OrderedMap[K, V]) Values() []V {
 func (om *OrderedMap[K, V]) Items() []*Entry[K, V] {
 	items := make([]*Entry[K, V], 0, om.Len())
 	for n := om.root.next; n != om.root; n = n.next {
-		items = append(items, newEntry(n.key, om.values[n.key]))
+		items = append(items, n.entry())
 	}
 	return items
 }
 
 // IterFunc returns an iterator function over contained items.
 func (om *OrderedMap[K, V]) IterFunc() func() (*Entry[K, V], bool) {
-	var curr *node[K]
 	root := om.root
-	curr = root.next
-	return func() (*Entry[K, V], bool) {
-		if curr == root {
-			return nil, false
+	curr := root.next
+	return func() (e *Entry[K, V], ok bool) {
+		if curr != root {
+			e = curr.entry()
+			curr = curr.next
+			return e, true
 		}
-		item := newEntry(curr.key, om.values[curr.key])
-		curr = curr.next
-		return item, true
+		return
 	}
 }
 
